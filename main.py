72f70dd18472f2f86cb2121c52cca8286143451d
Laacc <leitetunado2@outlook.com>
2026-08-30 14:25:19 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, redirect, url_for, request
+from manager import GerenciadorSenhas
+
+app = Flask(__name__)
+gerenciador = GerenciadorSenhas("data/senhas.db")
+
+
+@app.route("/")
+def pagina_inicial():
+    return render_template("index.html")
+
+@app.route("/gerar", methods=['GET', 'POST'])
+def pagina_gerar_senha():
+    if request.method == 'POST':
+        plataforma = request.form.get('plataforma')
+        senha = gerenciador.gerar_senha()
+        gerenciador.inserir_senha_db(plataforma, senha)
+        return redirect(url_for('pagina_consultar_senhas'))
+    else:
+        return render_template("gerar_senha.html")
+
+@app.route("/consultar")
+def pagina_consultar_senhas():
+    lista_senhas = gerenciador.carregar_senhas()
+    return render_template("consultar_senhas.html", senhas=lista_senhas)
+
+@app.route("/deletar", methods=['POST'])
+def deletar_senha():
+        plataforma = request.form.get('plataforma')
+        gerenciador.deletar_senha(plataforma)
+        return redirect(url_for('pagina_consultar_senhas'))
+
+@app.route("/trocar", methods=['POST'])
+def trocar_senha():
+     plataforma = request.form.get('plataforma')
+     gerenciador.trocar_senha(plataforma)
+     return redirect(url_for('pagina_consultar_senhas'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ manager.py
@@ -0,0 +1,59 @@
+import os
+import random
+import sqlite3
+import string
+
+class GerenciadorSenhas:
+    def __init__(self, caminho):
+        self.caminho = caminho
+        diretorio = os.path.dirname(self.caminho)
+        if diretorio:
+            os.makedirs(diretorio, exist_ok=True)
+        with sqlite3.connect(self.caminho) as con:
+            cursor = con.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS data (" \
+            "id INTEGER PRIMARY KEY," \
+            "plataforma TEXT," \
+            "senha TEXT)")
+            con.commit()
+
+    def gerar_senha(self, tamanho=14):
+        caracteres = string.ascii_letters + string.digits + string.punctuation
+        senha = "".join(random.choices(caracteres, k=tamanho))
+        return senha
+
+    def inserir_senha_db(self, plataforma, senha):
+        nova_senha = ((plataforma, senha))
+        with sqlite3.connect(self.caminho) as con:
+            cursor = con.cursor()
+            cursor.execute("INSERT INTO data (plataforma, senha) VALUES (?, ?)", nova_senha)
+            con.commit()
+    def carregar_senhas(self):
+        with sqlite3.connect(self.caminho) as con:
+            senhas = []
+            cursor = con.cursor()
+            cursor.execute("SELECT * FROM data ORDER BY id")
+            fetch = cursor.fetchall()
+            for linha in fetch:
+                senhas.append(linha)
+            return senhas
+
+    def trocar_senha(self, plataforma):
+        senha = self.gerar_senha()
+        nova_senha = ((senha, plataforma))
+        with sqlite3.connect(self.caminho) as con:
+            cursor = con.cursor()
+            cursor.execute("UPDATE data SET senha=(?) WHERE plataforma=(?)", nova_senha)
+            row_bool = cursor.rowcount
+            con.commit()
+            return row_bool == 1
+
+    def deletar_senha(self, plataforma):
+        plataforma_busca = ((plataforma,))
+        with sqlite3.connect(self.caminho) as con:
+            cursor = con.cursor()
+            cursor.execute("DELETE FROM data WHERE plataforma=(?)", plataforma_busca)
+            row_bool = cursor.rowcount
+            con.commit()
+            return row_bool == 1
+
